2b0453595ab10a41a5068fc78e1c84f500973dc3
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2013-10-15 11:20:07 +0300

    Store last github commit in `events` table

--- mooch/app.py
+++ mooch/app.py
@@ -1,19 +1,47 @@
 import sqlite3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+def init_db(db):
+
+    db.execute("""
+        CREATE TABLE if not exists events (
+            id INTEGER PRIMARY KEY,
+            eventType TEXT,
+            eventTime datetime,
+            data BLOB,
+            insertTime datetime default current_timestamp);
+        """)
+
+    # FIXME: not exists
+
+
 def get_db():
-    return sqlite3.connect(":memory:")
+    return sqlite3.connect("/tmp/mooch.sqlite", isolation_level=None)
 
 @app.route("/status")
 def status():
-    return jsonify({"okay": True,
-                    "GitHub": {"lastCommitTime": "2013-03-12T08:14:29-07:00"}})
+    status = {}
+    c = get_db().cursor()
+
+    # GitHub
+    c.execute(""" select max(eventTime) from events
+                  where eventType = 'github'  """)
+    last_github_ts = c.fetchone()[0]
+    if last_github_ts is not None:
+        status['GitHub'] = {"lastCommitTime": last_github_ts}
+
+    return jsonify(status)
 
 @app.route("/hooks/github", methods=["POST"])
 def github_hook():
-    pass
+    #import pdb; pdb.set_trace()
+    last_ts = max(commit['timestamp'] for commit in request.json['commits'])
+    get_db().execute("INSERT INTO events (eventType, eventTime) VALUES (?, ?)",
+                     ("github", last_ts))
+
 
 if __name__ == '__main__':
+    init_db(get_db())
     app.run(debug=True, host='0.0.0.0', port=5000)
