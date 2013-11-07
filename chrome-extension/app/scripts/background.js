92af72baa69471904e3dbffd70be2d06b7e1d7d3
mchernyavskaya <m.chernyavska@gmail.com>
2013-11-07 12:45:41 +0200

    moved status refresh into periodical action

--- chrome-extension/app/scripts/background.js
+++ chrome-extension/app/scripts/background.js
@@ -2,29 +2,30 @@
 
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
-});
-
-chrome.runtime.onLaunched.addListener(function () {
+    chrome.alarms.clearAll();
     chrome.alarms.create("moochStatusRefresh", {
-        when: Date.now(),
+        when: Date.now() + 1,
         periodInMinutes: 1
     });
 });
 
-chrome.runtime.onSuspend.addListener(function () {
-    console.log('cleaning up...');
-    chrome.alarms.clearAll();
-});
-
 chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log("on alarm", alarm);
     if (alarm.name == "moochStatusRefresh") {
+
         if (GitHub.hasLogin()) {
             console.log('requesting for GitHub...');
             GitHub.requestStatus();
         }
+        else {
+            console.log("No GitHub login.");
+        }
         if (CodeForces.hasLogin()) {
             console.log('requesting for CodeForces...');
             CodeForces.requestStatus();
+        }
+        else {
+            console.log("No CodeForces login.");
         }
     }
 });
