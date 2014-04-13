11d174a790c06e20079d041c6084103cbfdc2633
mchernyavskaya <m.chernyavska@gmail.com>
2014-04-13 14:28:02 +0300

    temporarily removed calendar fixed an error in popup status rendering

--- chrome-extension/app/scripts/popup.js
+++ chrome-extension/app/scripts/popup.js
@@ -1,13 +1,6 @@
 'use strict';
 var Popup = {
 
-    setPopupElementText: function (id, text) {
-        var htmlElement = document.getElementById(id);
-        if (htmlElement) {
-            htmlElement.innerText = text;
-        }
-    },
-
     render: function () {
         console.log('rerendering popup...');
         var bg = chrome.extension.getBackgroundPage();
@@ -17,36 +10,25 @@ var Popup = {
         }
         var sentinel = bg['MoochSentinel'];
         console.log('sentinel', sentinel);
-        this.setPopupElementText('status', '' + sentinel.statuses.codeForces);
-        this.setPopupElementText('ghStatus', '' + sentinel.statuses.gitHub);
-        var data = {
-            '1383926200': 10
-        };
-        this.renderCalendar(data);
+        this.setStatusText("status", sentinel.findStatus('codeForces'), "CodeForces puzzles");
+        this.setStatusText("ghStatus", sentinel.findStatus('gitHub'), "GitHub commits");
     },
 
-    renderCalendar: function (data) {
-        if (!this.cal) {
-          var startDate = new Date();
-          startDate.setMonth(startDate.getMonth() - 1);
-          this.cal = new CalHeatMap();
-          this.cal.init({
-              domain: 'month',
-              range: 2,
-              start: startDate,
-              cellSize: 10,
-              cellRadius: 2,
-              highlight: 'now',
-              data: data
-          });
-        } else {
-          this.cal.update(data);
+    setStatusText: function (id, status, label) {
+        if (!status) {
+            $("#" + id)[0].innerHTML = '';
+        }
+        else {
+            if (!label) {
+                label = status.name;
+            }
+            $("#" + id)[0].innerHTML = label + ": " + (status.value ? "You did it!" : "You mooch!");
         }
     },
 
     checkCodeForcesLogin: function () {
         console.log('check CodeForces login...');
-        var el = document.getElementById('status');
+        var el = $('#status')[0];
         if (!localStorage['moochLogin']) {
             el.innerText = 'Please set CodeForces user login on options page';
             return false;
@@ -56,7 +38,7 @@ var Popup = {
 
     checkGitHubLogin: function () {
         console.log('check GitHub login...');
-        var el = document.getElementById('ghStatus');
+        var el = $('#ghStatus')[0];
         if (!localStorage['gitHubLogin']) {
             el.innerText = 'Please set GitHub user login on options page';
             return false;
@@ -73,11 +55,10 @@ var Popup = {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("btnRefresh").onclick = function(e) {
-      MoochSentinel.updateStatuses();
-      Popup.refresh();
+    document.getElementById("btnRefresh").onclick = function () {
+        MoochSentinel.updateStatuses();
+        Popup.refresh();
     }
-
     Popup.refresh();
 });
 
